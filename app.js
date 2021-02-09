b7359fd42cc1a868749aecb8ec3ab82db51667fc
fajar muslim <13517149@std.stei.itb.ac.id>
2021-02-09 20:36:09 +0700

    deploy to heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const TelegramBot = require('node-telegram-bot-api');
 
-// replace the value below with the Telegram token you receive from @BotFather
+// rreplace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
