575dfec75405de068f653c0d3914313eda38ce69
fajar muslim <13517149@std.stei.itb.ac.id>
2021-02-10 11:46:21 +0700

    deploy to heroku

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
+/**
+ * This example demonstrates setting up webhook
+ * on the Heroku platform.
+ */
+
 require('dotenv').config({ silent: process.env.NODE_ENV === 'production' })
-
+const TOKEN = process.env.TOKEN
 const TelegramBot = require('node-telegram-bot-api');
-
-// replace the value below with the Telegram token you receive from @BotFather
-const token = process.env.TOKEN;
-
+const options = {
+    webHook: {
+        // Port to which you should bind is assigned to $PORT variable
+        // See: https://devcenter.heroku.com/articles/dynos#local-environment-variables
+        port: process.env.PORT
+        // you do NOT need to set up certificates since Heroku provides
+        // the SSL certs already (https://<app-name>.herokuapp.com)
+        // Also no need to pass IP because on Heroku you need to bind to 0.0.0.0
+    }
+};
+// Heroku routes from port :443 to $PORT
+// Add URL of your app to env variable or enable Dyno Metadata
+// to get this automatically
+// See: https://devcenter.heroku.com/articles/dyno-metadata
+const url = process.env.HEROKU_URL
 let bot;
 
 if (process.env.NODE_ENV === 'production') {
-    bot = new TelegramBot(token);
-    bot.setWebHook(process.env.HEROKU_URL + bot.token);
+    bot = new TelegramBot(TOKEN, options);
+    // This informs the Telegram servers of the new webhook.
+    // Note: we do not need to pass in the cert, as it already provided
+    bot.setWebHook(`${url}/bot${TOKEN}`);
 } else {
     // Create a bot that uses 'polling' to fetch new updates
-    bot = new TelegramBot(token, { polling: true });
+    bot = new TelegramBot(TOKEN, { polling: true });
 }
+
+
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -28,11 +48,10 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
     bot.sendMessage(chatId, resp);
 });
 
-// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
+// // Just to ping!
+// bot.on('message', function onMessage(msg) {
+//     const chatId = msg.chat.id;
 
-    // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message');
-});
+//     // send a message to the chat acknowledging receipt of their message
+//     bot.sendMessage(chatId, 'Received your message');
+// });
