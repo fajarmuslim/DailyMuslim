2a3c27a7de560c64137bcb8c96a9c37864d283f5
fajar muslim <13517149@std.stei.itb.ac.id>
2021-02-12 13:41:56 +0700

    add some boiler plate code

--- app.js
+++ app.js
@@ -33,7 +33,63 @@ if (process.env.NODE_ENV === 'production') {
     bot = new TelegramBot(TOKEN, { polling: true });
 }
 
+bot.on('message', (msg) => {
+    var hi = 'hi'
+    if (msg.text.toString().toLowerCase().indexOf(hi) === 0) {
+        bot.sendMessage(msg.from.id, "Hello " + msg.from.first_name + ", DailyMuslim siap menemani hari-harimu");
+    }
 
+    var bye = 'bye'
+    if (msg.text.toString().toLowerCase().indexOf(bye) === 0) {
+        bot.sendMessage(msg.chat.id, "Sampai jumpa kembali")
+    }
+
+    var html = 'html'
+    if (msg.text.toString().toLowerCase().indexOf(html) === 0) {
+        bot.sendMessage(msg.chat.id, "<b>bold</b> \n <i>italic</i> \n <em>italic with em</em> \n <a href=\"http://www.example.com/\">inline URL</a> \n <code>inline fixed-width code</code> \n <pre>pre-formatted fixed-width code block</pre>", { parse_mode: "HTML" });
+    }
+
+    var markdown = 'markdown'
+    if (msg.text.toString().toLowerCase().indexOf(markdown) === 0) {
+        bot.sendMessage(msg.chat.id, "*bold text* _italic text_  [text](http://www.example.com/)  `inline fixed-width code`", { parse_mode: "MARKDOWN" });
+    }
+
+    var location = "location";
+    if (msg.text.indexOf(location) === 0) {
+        bot.sendLocation(msg.chat.id, 44.97108, -104.27719);
+        bot.sendMessage(msg.chat.id, "Here is the point");
+
+    }
+
+    var location = "venue";
+    if (msg.text.indexOf(location) === 0) {
+        bot.sendVenue(msg.chat.id,
+            -6.8932055, 107.6107351,
+            'Institut Teknologi Bandung',
+            'Jl. Ganesha No.10, Lb. Siliwangi, Kecamatan Coblong, Kota Bandung, Jawa Barat 40132')
+    }
+
+    var contact = "contact";
+    if (msg.text.indexOf(contact) === 0) {
+        bot.sendContact(msg.chat.id, '085728341252', 'Fajar Muslim')
+    }
+})
+
+bot.onText(/\/start/, (msg) => {
+
+    bot.sendMessage(msg.chat.id, "Welcome", {
+        "reply_markup": {
+            "keyboard": [["Sample text", "Second sample"], ["Keyboard"], ["I'm robot"]]
+        }
+    });
+
+});
+
+bot.onText(/\/sendpic/, (msg) => {
+
+    bot.sendPhoto(msg.chat.id, "https://pbs.twimg.com/profile_images/836603072205856769/QtRFv6Jw_400x400.jpg", { caption: "Here we go ! \nThis is just a caption " });
+
+});
 
 // Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
@@ -47,11 +103,3 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
     // send back the matched "whatever" to the chat
     bot.sendMessage(chatId, resp);
 });
-
-// // Just to ping!
-// bot.on('message', function onMessage(msg) {
-//     const chatId = msg.chat.id;
-
-//     // send a message to the chat acknowledging receipt of their message
-//     bot.sendMessage(chatId, 'Received your message');
-// });
